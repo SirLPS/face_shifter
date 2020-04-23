fff10d29f72593e4b0bdb56ad78da86bd9cf4bf0
Pengshuo Li <xd13070110027@126.com>
2020-04-23 12:13:03 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -14,6 +14,48 @@ from torch.utils.data.dataloader import default_collate
 import random
 
 
+class FaceEmbed(TensorDataset):
+    def __init__(self, data_path_list, same_prob=0.8):
+        datasets = []
+        # embeds = []
+        self.N = []
+        self.same_prob = same_prob
+        for data_path in os.listdir(data_path_list):
+            image_list = glob.glob('{:s}/{:s}/*.jpg'.format(data_path_list,data_path))
+            datasets.append(image_list)
+            self.N.append(len(image_list))
+
+        self.datasets = datasets
+        self.transforms = transforms.Compose([
+            transforms.ColorJitter(0.2, 0.2, 0.2, 0.01),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+    def __getitem__(self, item):
+        idx = 0
+        while item >= self.N[idx]:
+            item -= self.N[idx]
+            idx += 1
+        image_path = self.datasets[idx][item]
+
+        Xs = cv2.imread(image_path)
+        Xs = Image.fromarray(Xs)
+
+        if random.random() > self.same_prob:
+            image_path = random.choice(self.datasets[random.randint(0, len(self.datasets)-1)])
+            Xt = cv2.imread(image_path)
+            Xt = Image.fromarray(Xt)
+            same_person = 0
+        else:
+            Xt = Xs.copy()
+            same_person = 1
+        return self.transforms(Xs), self.transforms(Xt), same_person
+
+    def __len__(self):
+        return sum(self.N)
+
+    
 def my_collate_fn(batch):
     """
     detect_align data to form a batch
